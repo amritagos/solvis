969bb424ec039b072bc348967f98a13a238d9bc4
Amrita Goswami <amrita16thaug646@gmail.com>
2024-02-28 16:04:35 +0000

    Bug fix: in creating solvation shell from center

--- examples/render_capped_trigonal_prism/render_ctp.py
+++ examples/render_capped_trigonal_prism/render_ctp.py
@@ -30,45 +30,24 @@ atom_radius = 0.1
 
 # Read in the current frame 
 currentframe = read(infilename, format="lammps-dump-text") # Read in the last frame of the trajectory
-
-pos = currentframe.get_positions()
-
-# Make a "fake center"
-fake_center = np.mean(pos, axis=0)
-# Box size lengths 
 box_len = currentframe.get_cell_lengths_and_angles()[:3]
-# 7 Nearest neighbours
-k=7
-dist, neigh_ind = solvis.util.k_nearest_neighbours(pos, fake_center, k, box_len)
-k_near_pos = pos[neigh_ind]
 
-# Get the convex hull using the scipy wrapper for QHull
-hull = ConvexHull(k_near_pos)
-# Number of faces, which is also the number of cells in the subsequent polyhull
-num_faces = len(hull.simplices)
- 
-area = hull.area
-vol = hull.volume 
+# Create a SolvationShell object from the solvent 
+solvation_shell = solvis.solvation_shell.create_solvation_shell_from_solvent(currentframe, box_len, center=None)
+pos = solvation_shell.atoms.get_positions()
+# Get the distance of the seven neighbours, from the center
+dist = solvation_shell.distances_of_k_neighbours_from_center(num_neighbours=7,coordinating_type='all')
 
+convex_hull = solvation_shell.build_convex_hull_k_neighbours(num_neighbours=7) 
 # Calculate the sphericity from the volume and area 
-sph_value = solvis.util.sphericity(vol,area)
+sph_value = solvis.util.sphericity(convex_hull.volume,convex_hull.area)
 print('The calculated sphericity with 7 neighbours is', sph_value, '\n')
 
 # Convert the convex hull into a pyvista PolyData object
-faces_pyvistaformat = np.column_stack((3*np.ones((len(hull.simplices), 1), dtype=int), hull.simplices)).flatten()
-polyhull = PolyData(k_near_pos, faces_pyvistaformat)
+polyhull = convex_hull.pyvista_hull_from_convex_hull()
 
-faces = hull.simplices
-
-# Get edges as pairs of indices based on the faces
-edges = set()
-
-for simplex in faces:
-    edges.add(tuple(sorted([simplex[0], simplex[1]])))
-    edges.add(tuple(sorted([simplex[1], simplex[2]])))
-    edges.add(tuple(sorted([simplex[2], simplex[0]])))
-
-edges = np.array(list(edges))
+# Get a numPy array of edges (n_edges,2)
+edges = convex_hull.get_edges()
 
 # ------------------------------------------------------------
 
@@ -81,32 +60,32 @@ pl_inter = AtomicPlotter(interactive_mode=True, depth_peeling=True, shadows=True
 # Add the hull as a mesh 
 pl_inter.add_hull(polyhull,cmap=mesh_cmap,clim=[dist[4],dist[-1]],scalars=dist)
 # Create the bonds corresponding to the edges and add them to the plotter
-pl_inter.create_bonds_from_edges(k_near_pos, edges, point_colors=point_colours,
+pl_inter.create_bonds_from_edges(pos, edges, point_colors=point_colours,
     radius=bond_radius, resolution=1,bond_gradient_start=bond_gradient_start)
 # Add the atoms as spheres
-pl_inter.add_atoms_as_spheres(k_near_pos, point_colours, radius=atom_radius)
+pl_inter.add_atoms_as_spheres(pos, point_colours, radius=atom_radius)
 # Remove bonds or atoms interactively
 pl_inter.interactive_window(delete_actor=True) 
 
-# ---------------------------
-# Now render the image (offscreen=True)
+# # ---------------------------
+# # Now render the image (offscreen=True)
 
-pl_render = AtomicPlotter(interactive_mode=False, window_size=[4000,4000], depth_peeling=True, shadows=True)
-# Add the hull as a mesh 
-pl_render.add_hull(polyhull, cmap=mesh_cmap,clim=[dist[4],dist[-1]],scalars=dist)
-# Create the bonds corresponding to the edges and add them to the plotter
-pl_render.create_bonds_from_edges(k_near_pos, edges, point_colors=point_colours,
-    radius=bond_radius, resolution=1,bond_gradient_start=bond_gradient_start)
-# Delete actors selected interactively 
-pl_render.plotter.remove_actor(pl_inter.selected_actors)
-# Add the atoms as spheres
-pl_render.add_atoms_as_spheres(k_near_pos, point_colours, radius=atom_radius)
-# Get and set the camera position found from the last frame in the interactive mode
-inter_cpos = pl_inter.interactive_camera_position[-1]
-print("Camera position will be set to ", inter_cpos)
-# Save image 
-pl_render.render_image(imagename, inter_cpos)
+# pl_render = AtomicPlotter(interactive_mode=False, window_size=[4000,4000], depth_peeling=True, shadows=True)
+# # Add the hull as a mesh 
+# pl_render.add_hull(polyhull, cmap=mesh_cmap,clim=[dist[4],dist[-1]],scalars=dist)
+# # Create the bonds corresponding to the edges and add them to the plotter
+# pl_render.create_bonds_from_edges(pos, edges, point_colors=point_colours,
+#     radius=bond_radius, resolution=1,bond_gradient_start=bond_gradient_start)
+# # Delete actors selected interactively 
+# pl_render.plotter.remove_actor(pl_inter.selected_actors)
+# # Add the atoms as spheres
+# pl_render.add_atoms_as_spheres(pos, point_colours, radius=atom_radius)
+# # Get and set the camera position found from the last frame in the interactive mode
+# inter_cpos = pl_inter.interactive_camera_position[-1]
+# print("Camera position will be set to ", inter_cpos)
+# # Save image 
+# pl_render.render_image(imagename, inter_cpos)
 
-# Trim the image
-trimmed_im = solvis.util.trim(imagename,10)
-trimmed_im.save(trimmed_img_name)
+# # Trim the image
+# trimmed_im = solvis.util.trim(imagename,10)
+# trimmed_im.save(trimmed_img_name)

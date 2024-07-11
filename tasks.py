75d203a4f84d7d3030b1c2b9dfd6da317dce1a92
Amrita Goswami <amrita16thaug646@gmail.com>
2024-07-11 14:37:46 +0000

    Installation: Use meson-python instead
    Now we use `meson-python` to build the package using `pip install`. The
    `environment.yml` had to be changed to include `meson-python` as a
    dependency.
    Modified the CI as well to reflect this change.

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke import task
-
-@task
-def build(ctx, install=False):
-    print("Building the extension")
-    if install:
-        ctx.run("flit install")
-    else:
-        ctx.run("flit build")

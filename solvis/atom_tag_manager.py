7bb33b8fadeaf98e92c180bace01c043b2b656db
Amrita Goswami <amrita16thaug646@gmail.com>
2024-07-16 11:59:17 +0000

    atom_tag_manager: Better docs and type hints
    Added more documentation of functions and added type hints

--- solvis/atom_tag_manager.py
+++ solvis/atom_tag_manager.py
@@ -2,31 +2,50 @@ from ase import Atoms
 
 
 class AtomTagManager:
-    def __init__(self, atoms):
+    def __init__(self, atoms: Atoms):
+
         self.tag_index_mapping = self._create_tag_index_mapping(atoms)
 
-    def _create_tag_index_mapping(self, atoms):
-        """
-        Create a dictionary mapping tags to indices and vice versa in the Atoms object.
+    def _create_tag_index_mapping(self, atoms: Atoms) -> dict[int, int]:
+        """Create a dictionary mapping tags to indices and vice versa in the Atoms object.
+
+        Args:
+            atoms (Atoms): The ASE atoms object
+
+        Returns:
+            dict[int, int]: Key: tag, value: index
         """
         return {atom.tag: (index, atom.tag) for index, atom in enumerate(atoms)}
 
-    def update_tag_index_mapping(self, atoms):
-        """
-        Update the dictionary mapping tags to indices and vice versa based on the order of Atom objects in the Atoms object.
+    def update_tag_index_mapping(self, atoms: Atoms) -> None:
+        """Update the dictionary mapping tags to indices and vice versa based on the order of Atom objects in the Atoms object.
+
+        Args:
+            atoms (Atoms): The ASE Atoms object
         """
         self.tag_index_mapping = {
             atom.tag: (index, atom.tag) for index, atom in enumerate(atoms)
         }
 
-    def lookup_index_by_tag(self, tag):
+    def lookup_index_by_tag(self, tag: int) -> int:
+        """Look up the index of the Atom object with the specified tag.
+
+        Args:
+            tag (int): Desired tag
+
+        Returns:
+            int: Index corresponding to the input tag
         """
-        Look up the index of the Atom object with the specified tag.
-        """
+
         return self.tag_index_mapping.get(tag, (None, None))[0]
 
-    def lookup_tag_by_index(self, index):
-        """
-        Look up the tag of the Atom object at the specified index.
+    def lookup_tag_by_index(self, index: int) -> int:
+        """Look up the tag of the Atom object at the specified index.
+
+        Args:
+            index (int): index for which you want the tag
+
+        Returns:
+            int: tag corresponding to the input index
         """
         return self.tag_index_mapping.get(index, (None, None))[1]
